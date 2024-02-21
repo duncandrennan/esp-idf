a462c7b3e24b4e0705e01b44f5e914228518adc8
Marius Bensch <mabensch@gmx.net>
2024-02-21 22:44:39 +0200

    function adc_digi_suspend()

--- components/driver/adc.c
+++ components/driver/adc.c
@@ -347,6 +347,11 @@ static IRAM_ATTR void adc_dma_intr_handler(void *arg)
 extern QueueHandle_t g_adc_data_queue;
 dma_descriptor_t g_descriptor;
 
+IRAM_ATTR void adc_digi_suspend(void)
+{
+    adc_hal_digi_suspend(&s_adc_digi_ctx->hal);
+}
+
 static IRAM_ATTR bool s_adc_dma_intr(adc_digi_context_t *adc_digi_ctx)
 {
     portBASE_TYPE taskAwoken = 0;
